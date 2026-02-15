e2d90c39c68b2ddfabd02a4740525c3ca9b7dc1a
Anton Sivertsson <sivertsson.anton@gmail.com>
2026-02-15 18:03:24 +0100

    typepipe v0.0.2

--- Formula/typepipe.rb
+++ Formula/typepipe.rb
@@ -7,10 +7,12 @@ class Typepipe < Formula
   env :std
   desc "Spotlight like application for personal use"
   homepage "https://github.com/antonsivertsson/typepipe"
-  head "https://github.com/antonsivertsson/typepipe.git", branch: "main"
+  url "https://github.com/antonsivertsson/typepipe/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "2805603d7243aeeadd6422c3c7a555bbe3fb0dbd8d676b1dda7826b6f7981db2"
+  version "0.0.2"
 
   def install
-    system "swiftc", 'Typepipe.swift', '-o', 'typepipe'
+    system "swiftc", 'main.swift Version.swift', '-o', 'typepipe'
     bin.install "#{buildpath}/typepipe"
   end
 end
