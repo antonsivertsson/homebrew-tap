419c3886044251a0d36b9d5e8238557032f9f61b
Anton Sivertsson <sivertsson.anton@gmail.com>
2026-02-16 10:57:28 +0100

    typepipe - update formula order

--- Formula/typepipe.rb
+++ Formula/typepipe.rb
@@ -5,10 +5,10 @@
 # typepipe.rb
 class Typepipe < Formula
   env :std
+  desc "Spotlight like application for personal use"
   depends_on :macos
   depends_on macos: :sonoma
   depends_on "swift"
-  desc "Spotlight like application for personal use"
   homepage "https://github.com/antonsivertsson/typepipe"
   url "https://github.com/antonsivertsson/typepipe/archive/refs/tags/v0.0.2.tar.gz"
   sha256 "2805603d7243aeeadd6422c3c7a555bbe3fb0dbd8d676b1dda7826b6f7981db2"
