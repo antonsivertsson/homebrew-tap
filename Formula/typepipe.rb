e0d714d10aec13e8f5aae22bf8688970757d2ccc
Anton Sivertsson <sivertsson.anton@gmail.com>
2026-02-16 11:02:54 +0100

    Typepipe - fix class property orders

--- Formula/typepipe.rb
+++ Formula/typepipe.rb
@@ -6,12 +6,12 @@
 class Typepipe < Formula
   env :std
   desc "Spotlight like application for personal use"
-  depends_on :macos
-  depends_on macos: :sonoma
-  depends_on "swift"
   homepage "https://github.com/antonsivertsson/typepipe"
   url "https://github.com/antonsivertsson/typepipe/archive/refs/tags/v0.0.2.tar.gz"
   sha256 "2805603d7243aeeadd6422c3c7a555bbe3fb0dbd8d676b1dda7826b6f7981db2"
+  depends_on :macos
+  depends_on macos: :sonoma
+  depends_on "swift"
 
   def install
     system "swiftc", "main.swift", "Version.swift", "-o", "typepipe"
