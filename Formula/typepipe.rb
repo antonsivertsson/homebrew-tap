60cb87e266d7df123777830ba03d5d2d1d137312
Anton Sivertsson <sivertsson.anton@gmail.com>
2026-02-16 10:38:20 +0100

    Typepipe - Update formula

--- Formula/typepipe.rb
+++ Formula/typepipe.rb
@@ -9,10 +9,9 @@ class Typepipe < Formula
   homepage "https://github.com/antonsivertsson/typepipe"
   url "https://github.com/antonsivertsson/typepipe/archive/refs/tags/v0.0.2.tar.gz"
   sha256 "2805603d7243aeeadd6422c3c7a555bbe3fb0dbd8d676b1dda7826b6f7981db2"
-  version "0.0.2"
 
   def install
-    system "swiftc", 'main.swift', 'Version.swift', '-o', 'typepipe'
+    system "swiftc", "main.swift", "Version.swift", "-o", "typepipe"
     bin.install "#{buildpath}/typepipe"
   end
 end
